43cb2edb17bea0f17daf7baa53c9bcb4bd480b6b
Utkarsh Singh <businessutkarsh22@gmail.com>
2025-04-08 20:43:07 +0530

    sec
    dsds

--- erpnext/tests/test_activation.py
+++
@@ -1,9 +0,0 @@
-from frappe.tests import IntegrationTestCase
-
-from erpnext.utilities.activation import get_level
-
-
-class TestActivation(IntegrationTestCase):
-	def test_activation(self):
-		levels = get_level()
-		self.assertTrue(levels)
